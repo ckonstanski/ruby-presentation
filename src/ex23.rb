8c0289ecd60662d316d5a6429ec0f4f5790a360a
ckonstanski <ckonstanski@pippiandcarlos.com>
2017-01-03 20:21:46 -0700

    Added threading slides

---
+++ src/ex23.rb
@@ -0,0 +1,28 @@
+# ex23.rb
+require 'thread'
+
+$mutex = Mutex.new
+$global_counter = 0
+
+def do_thread(name)
+    $global_counter += 1
+    sleep $global_counter
+    puts "#{name}: global_counter now at: #{$global_counter}"
+end
+
+puts "Before any threads are spun up..."
+t1 = Thread.new {
+    $mutex.synchronize {
+        do_thread("Thread 1")
+    }
+}
+t2 = Thread.new {
+    $mutex.synchronize {
+        do_thread("thread 2")
+    }
+}
+puts "After the threads are spun up but while they are still sleeping..."
+t1.join
+puts "Thread 1 is done"
+t2.join
+puts "Thread 2 is done"
